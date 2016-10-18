e462529972d3be72d84821dd7c075eaa2dbb2d52
Christian MilNeil <c.neal.milneil@gmail.com>
2016-10-18 16:36:54 -0400

    Colors

--- projects/pyramids/pyramids.js
+++ projects/pyramids/pyramids.js
@@ -8,13 +8,14 @@ function drawPyramids(width, height, canvas) {
 
   context.save();
   context.clearRect(0, 0, width, height);
-  context.fillStyle = "#fff";
+
   context.translate(width / 2, height / 2);
   for (var x = 10, d; x >= -20; --x) {
     for (var y = 10; y >= -10; --y) {
       if ((d = distanceManhattan(x, y)) > 17) continue;
       var te = d3_ease.easeCubic(1);
-      drawCube((d & 1 ? -1 : +1) * (Math.PI / 4 - te * Math.PI / 2), x * scalar, y * scalar, -3 * scalar * te + 2 * scalar * Math.pow(te, 2));
+      var f = 'rgb(' + (80 + 6*x) + "," + 8*Math.abs(y) + ",90)";
+      drawCube((d & 1 ? -1 : +1) * (Math.PI / 4 - te * Math.PI / 2), f, x * scalar, y * scalar, -3 * scalar * te + 2 * scalar * Math.pow(te, 2));
     }
   }
   var elapsed = 0, 
@@ -24,23 +25,27 @@ function drawPyramids(width, height, canvas) {
 
   window.addEventListener('scroll', function() { if (!bouncing) {return bounce();} });
 
+  var iter=1;
+
   function bounce() {
+    iter++;
     context.restore();
     timer.restart(function(elapsed) {
       // console.log(elapsed);
       context.save();
-      context.clearRect(0, 0, width, height);
       context.fillStyle = "#fff";
+      context.clearRect(0, 0, width, height);
       context.translate(width / 2, height / 2);
-      for (var x = 10, d, t = (elapsed / 2500); x >= -10; --x) {
+      for (var x=10, d, t = (elapsed / 2500); x >= -10; --x) {
         for (var y = 10; y >= -10; --y) {
           if ((d = distanceManhattan(x, y)) > 17) continue;
           var te = d3_ease.easeCubic(Math.max(0, Math.min(1, t * 3.8 - distanceCartesian(x, y) / 4)));
-          drawCube((d & 1 ? -1 : +1) * (Math.PI / 4 - te * Math.PI / 2), x * scalar, y * scalar, -3 * scalar * te + 2 * scalar * Math.pow(te, 2));
+          var f = 'rgb(' + (100 + 30*(x+iter)) + "," + 20*Math.abs(y+iter) + "," + (5*(y+iter)*(x+iter)) + ")";
+          drawCube((d & 1 ? -1 : +1) * (Math.PI / 4 - te * Math.PI / 2), f, x * scalar, y * scalar, -3 * scalar * te + 2 * scalar * Math.pow(te, 2));
         }
       }
       context.restore();
-      if (elapsed > 5000) { bouncing = false; timer.stop(); } else { bouncing = true; }
+      if (elapsed > 2500) { bouncing = false; timer.stop(); } else { bouncing = true; }
     }, 0);
     return true;
   }
@@ -53,7 +58,7 @@ function drawPyramids(width, height, canvas) {
     return Math.abs(x) + Math.abs(y);
   }
 
-  function drawCube(angle, x, y, z) {
+  function drawCube(angle, fill, x, y, z) {
 
   // isometric coordinate system:
   // +y  \/  +x
@@ -64,6 +69,7 @@ function drawPyramids(width, height, canvas) {
     isocontext.translate3d(x, y, z);
     isocontext.rotateZ(angle - Math.PI / 4);
 
+    context.fillStyle = fill;
     context.beginPath();
     isocontext.moveTo(+1.5, -1.5, -1.5);
     isocontext.lineTo(-0.0, +0.0, +1.5);  // top vertex
